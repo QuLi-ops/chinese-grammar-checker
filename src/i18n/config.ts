bd7638bf90aa0c882fe6309e2b6dc1745699f3d8
QuLi-ops <quli1016908036@gmail.com>
2025-02-26 19:37:23 +0800

    解决异步问题

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,5 +1,16 @@
 import {getRequestConfig} from 'next-intl/server';
 
-export default getRequestConfig(async ({locale}) => ({
-  messages: (await import(`../messages/${locale}/common.json`)).default
-})); 
+export default getRequestConfig(async ({requestLocale}) => {
+  // 获取请求的 locale，需要 await
+  let locale = await requestLocale;
+  
+  // 确保 locale 有效，如果无效则使用默认值
+  if (!locale || !['en', 'zh'].includes(locale)) {
+    locale = 'en';
+  }
+  
+  return {
+    locale,
+    messages: (await import(`../messages/${locale}/common.json`)).default
+  };
+}); 
